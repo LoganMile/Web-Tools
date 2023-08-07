c1654837908266e4a16818c1849d7c21a08909d5
LoganMile <140265722+LoganMile@users.noreply.github.com>
2023-08-07 20:58:06 +0100

    Add files via upload

---
+++ Animate.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var content = document.getElementById("content");
+    content.style.display = "block";
+    content.style.transform = "translateY(0)";
+  });
